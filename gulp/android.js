46ebef52554130a7bf0580cd80d2300e5dcd8203
Matěj Kříž <HAL4200@Matej-MacBook-Pro.local>
2017-01-04 16:01:38 +0100

    align with este -> fix for windows

--- gulp/android.js
+++ gulp/android.js
@@ -1,4 +1,7 @@
 import bg from 'gulp-bg';
 import gulp from 'gulp';
 
-gulp.task('android', ['native', 'android-simulator'], bg('react-native', 'run-android'));
+gulp.task('android', ['native', 'android-simulator'], bg(
+  'node', 'node_modules/react-native/local-cli/cli.js',
+  'run-android',
+));
