b7c370594cf81eb5aa75f7526a6c5fdf189504dd
Matej Kriz <matej.kriz@actum.cz>
2017-01-09 19:49:52 +0100

    fix merge - stop reverting changes

--- src/common/configureReducer.js
+++ src/common/configureReducer.js
@@ -1,5 +1,4 @@
 // @flow weak
-import type { Action, State } from './types';
 import app from './app/reducer';
 import config from './config/reducer';
 import device from './device/reducer';
@@ -7,40 +6,12 @@ import intl from './intl/reducer';
 import { combineReducers } from 'redux';
 import { fieldsReducer as fields } from './lib/redux-fields';
 
-// stackoverflow.com/q/35622588/233902
-const resetStateOnSignOutReducer = (reducer, initialState) => (
-  state: State,
-  action: Action,
-) => {
-  const userWasSignedOut =
-    action.type === 'ON_AUTH' &&
-    state.users.viewer &&
-    !action.payload.firebaseUser;
-  if (!userWasSignedOut) {
-    return reducer(state, action);
-  }
-  // Reset app state to purge all sensitive data.
-  return reducer({
-    app: state.app,
-    config: initialState.config,
-    device: initialState.device,
-    intl: initialState.intl,
-  }, action);
-};
-
-const configureReducer = (initialState: Object) => {
-  let reducer = combineReducers({
-    app,
-    config,
-    device,
-    fields,
-    intl,
-  });
-
-  // The power of higher-order reducers, http://slides.com/omnidan/hor
-  reducer = resetStateOnSignOutReducer(reducer, initialState);
-
-  return reducer;
-};
+const configureReducer = () => combineReducers({
+  app,
+  config,
+  device,
+  fields,
+  intl,
+});
 
 export default configureReducer;
