bc9cd57e56ceb333f4fa4e38e0d7ffdb8ccb00e7
Matěj Kříž <matej.kriz@actum.cz>
2017-04-11 13:48:02 +0200

    clean config

--- src/server/config.js
+++ src/server/config.js
@@ -6,7 +6,7 @@ import nconf from 'nconf';
 nconf.env('__');
 
 // For local development, we can override defaults easily. Rename
-// src/common/_config.json to src/common/config.json and uncomment next line.
+// src/common/config.example.json to src/common/config.json and uncomment next line.
 // nconf.file('src/common/config.json');
 
 // Remember, never put secrets in the source code. Use environment variables for
@@ -16,24 +16,10 @@ nconf.defaults({
   // Use appVersion defined in gulp env task or Heroku dyno metadata.
   appVersion: process.env.appVersion || process.env.HEROKU_SLUG_COMMIT,
   defaultLocale: 'en',
-  firebase: {
-    // To get the config, just click Add web app from the overview page.
-    apiKey: 'AIzaSyDZRAOrDErAaC-TCKbr4cMzaohsPR4sWgU',
-    authDomain: 'este.firebaseapp.com',
-    databaseURL: 'https://este.firebaseio.com',
-    storageBucket: 'project-808488257248094054.appspot.com',
-  },
-  googleAnalyticsId: 'UA-XXXXXXX-X',
   isProduction: process.env.NODE_ENV === 'production',
   locales: ['cs', 'en'],
   nextjsDir: 'src',
   port: process.env.PORT || 3000,
-  // Enable hot reload on remote device. Note it prevents offline testing,
-  // because it depends on ip.address(), which doesn't work with disabled wifi.
-  // How do we access a website running on localhost from mobile browser?
-  // stackoverflow.com/questions/3132105
-  remoteHotReload: true,
-  sentryUrl: 'https://f297cec9c9654088b8ccf1ea9136c458@app.getsentry.com/77415',
 });
 
 export default nconf.get();
